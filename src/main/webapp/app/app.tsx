826dddfda74c9413a9da7388e823932a9373fa5d
Wube <wubshet.kifle@gsa.gov>
2021-01-11 13:59:13 -0500

    Header and footer added

--- src/main/webapp/app/app.tsx
+++ src/main/webapp/app/app.tsx
@@ -5,10 +5,10 @@ import { Card } from 'reactstrap';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { ToastContainer, toast } from 'react-toastify';
 import { hot } from 'react-hot-loader';
-import Header from 'app/shared/layout/header/header';
-import Footer from 'app/shared/layout/footer/footer';
-import ErrorBoundary from 'app/shared/error/error-boundary';
-import AppRoutes from 'app/routes';
+import Header from '../app/shared/layout/header/header';
+import Footer from '../app/shared/layout/footer/footer';
+import ErrorBoundary from '../app/shared/error/error-boundary';
+import AppRoutes from '../app/routes';
 
 const baseHref = document
   .querySelector('base')
@@ -16,22 +16,25 @@ const baseHref = document
   .replace(/\/$/, '');
 
 export const App = () => {
-  const paddingTop = '60px';
   return (
     <Router basename={baseHref}>
-      <div className="app-container" style={{ paddingTop }}>
+      <div className="app-container">
         <ToastContainer position={toast.POSITION.TOP_LEFT} className="toastify-container" toastClassName="toastify-toast" />
-        <ErrorBoundary>
-          <Header />
-        </ErrorBoundary>
-        <div className="container-fluid view-container" id="app-view-container">
-          <Card className="jh-card">
-            <ErrorBoundary>
-              <AppRoutes />
-            </ErrorBoundary>
-          </Card>
-          <Footer />
-        </div>
+        <Header />
+        <main className="main-content background pt-3 pb-3" aria-label="Content">
+          <div>
+            <div className="grid-container">
+              <div className="grid-row">
+                <div className="grid-col-12">
+                  <ErrorBoundary>
+                    <AppRoutes />
+                  </ErrorBoundary>
+                </div>
+              </div>
+            </div>
+          </div>
+        </main>
+        <Footer />
       </div>
     </Router>
   );
--- src/main/webapp/app/shared/layout/footer/footer.tsx
+++ src/main/webapp/app/shared/layout/footer/footer.tsx
@@ -1,15 +1,23 @@
 import './footer.scss';
 import React from 'react';
-import { Col, Row } from 'reactstrap';
-
+/* eslint @typescript-eslint/no-var-requires: "off" */
+const faasLogo = require('../../../../content/images/faas-logo.png');
 const Footer = () => (
-  <div className="footer page-content">
-    <Row>
-      <Col md="12">
-        <p>Footer goes here</p>
-      </Col>
-    </Row>
-  </div>
+  <footer className="usa-footer">
+    <div className="usa-footer__secondary-section">
+      <div className="grid-container">
+        <div className="grid-row grid-gap">
+          <div className="usa-footer__logo grid-row mobile-lg:grid-col-6 mobile-lg:grid-gap-2">
+            <div className="mobile-lg:grid-col-auto">
+              <p className="usa-text-small">Powered by</p>
+            </div>
+            <div className="mobile-lg:grid-col-auto">
+              <img src={faasLogo} role="img" alt="" style={{ marginTop: '-20px' }} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
 );
-
 export default Footer;
